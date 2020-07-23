798af32178a7dcd2f8817bf3b35125104fb5c48e
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-07-23 17:11:14 -0600

    committing auth required endpoint for plants router

--- plants/plants-router.js
+++ plants/plants-router.js
@@ -1,21 +1,11 @@
 const axios = require('axios');
-const express = require('express');
-const server = express();
+
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to our API',
+    message: 'Welcome to the plants router!',
   });
-
-  axios
-    .get('http://localhost:4000/api/plants')
-    .then((response) => {
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      res.status(500).json({ message: 'Error Fetching Plants', error: err });
-    });
 });
 
 module.exports = router;

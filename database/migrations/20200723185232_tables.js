ad5ba201df7311a37bfc382b0ec8e8acd92a440b
Damaged0ne <43980544+Damaged0ne@users.noreply.github.com>
2020-07-23 19:02:18 -0600

    created a  migration for both users and plants tables with fk

---
+++ database/migrations/20200723185232_tables.js
@@ -0,0 +1,28 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('users', (users) => {
+      users.increments().notNullable().primary();
+      users.string('username', 255).notNullable().unique();
+      users.string('password', 255).notNullable();
+      users.integer('phoneNumber').notNullable();
+    })
+    .createTable('plants', (plants) => {
+      plants.increments();
+      plants.string('nickname', 255).notNullable().unique();
+      plants.string('species', 255).notNullable().unique();
+      plants.integer('h2oFrequency').notNullable();
+      plants.string('image', 255).unique();
+      plants
+        .integer('user_id')
+        .unsigned()
+        .notNullable()
+        .references('id')
+        .inTable('users')
+        .onUpdate('CASCADE')
+        .onDelete('CASCADE');
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users').dropTableIfExists('plants');
+};
--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,22 @@
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
-    useNullAsDefault: true,
+    useNullAsDefault: true, // needed for sqlite
+    connection: {
+      filename: './database/auth.db3',
+    },
     migrations: {
       directory: './database/migrations',
-      tableName: 'dbmigrations',
     },
-    seeds: { directory: './database/seeds' },
+    seeds: {
+      directory: './database/seeds',
+    },
+    // add the following
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
   },
 };

7d833ca93d5ed01c560708652c492766f01c9fbe
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-07-26 21:46:29 -0400

    Post Talk with Kristin

--- plant/plantsRouters.js
+++ plant/plantsRouters.js
@@ -8,24 +8,6 @@ const router = express.Router();
 
 // Get All of a specific users plants
 
-// router.get("/user/:userid/plants", restrict(), async (req, res) => {
-//   const { userid } = req.params;
-//   entry.plants.user_id = userid;
-//   Users.findById(userid).then((user) => {
-//     if (user) {
-//       Plants.getAllPlants(userid)
-//         .then((plants) => {
-//           res.status(200).json(plants);
-//         })
-//         .catch((err) => {
-//           res.status(500).json(err);
-//         });
-//     } else {
-//       res.status(400).json({ message: "User does not exist" });
-//     }
-//   });
-// });
-
 router.get(
   "/users/:id/plants",
   validateUserId(),

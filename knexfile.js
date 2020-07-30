723c3c1ef5478e29eb036f2394fa062a9afd7bcb
Chris Daniels <43980544+ChrisRDaniels@users.noreply.github.com>
2020-07-29 18:04:17 -0600

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,19 @@
 // Update with your config settings.
 
 module.exports = {
+     production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './database/migrations'
+    }
+  }
+}
+,
   development: {
     client: "sqlite3",
     connection: { filename: "./database/auth.db3" },
@@ -15,19 +28,6 @@ module.exports = {
     afterCreate: (conn, done) => {
       // runs after a connection is made to the sqlite engine
       conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
-    },
-    production: {
-    client: "pg",
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: './database/migrations'
     }
-  }
-};
-
   },
 };

eaf072b067ad3a57593cb244ee8b021f2b9937ac
Henry Brophy <henrybrophy@henrys-mbp.lan>
2020-07-31 09:12:53 -0400

    Refactored the authRouter test.

--- authentification/authRouters.spec.js
+++ authentification/authRouters.spec.js
@@ -2,31 +2,17 @@ const request = require("supertest");
 const server = require("../api/server");
 
 describe("POST /register", () => {
-  it("should return a 201 Created status code", async () => {
+  it("Status Code, json,username", async () => {
     const response = await request(server).post("/register").send({
-      username: "Mike",
+      username: "Reggie1",
       password: "Password2",
       phoneNumber: 8675309,
     });
     expect(response.statusCode).toBe(201);
-  });
 
-  it("should return json", async () => {
-    const response = await request(server).post("/register").send({
-      username: "Tommy",
-      password: "Password2",
-      phoneNumber: 8675309,
-    });
     expect(response.type).toMatch(/json/i);
-  });
 
-  it("returning username", async () => {
-    const response = await request(server).post("/register").send({
-      username: "Bobby",
-      password: "Password2",
-      phoneNumber: 8675309,
-    });
-    expect(response.body.username).toBe("Bobby");
+    expect(response.body.username).toBe("Reggie1");
   });
 });
 
@@ -34,7 +20,7 @@ describe("POST /login", () => {
   it("should allow a registered user to log in", async () => {
     const response = await request(server)
       .post("/login")
-      .send({ username: "Tim", password: "Password2" });
+      .send({ username: "Reggie1", password: "Password2" });
     expect(response.status).toBe(200);
   });
 });
--- plant/plants.spec.js
+++ plant/plants.spec.js
@@ -1,22 +1,22 @@
 const request = require("supertest");
 const server = require("../api/server");
-const db = require("../database/config");
 require("dotenv").config();
 
 describe("GET /users/:id/plants", () => {
-  it("should return 200 http status code", async () => {
+  it("should return 200 JSON", async () => {
     await request(server)
       .post("/login")
       .send({ username: "Henry", password: "password" })
       .then(async (res) => {
-        console.log(res.body, "res");
+        // console.log(res.body, "res");
         await request(server)
           .get(`/users/${res.body.userId}/plants`)
           .set("token", res.body.token)
 
           .then((response) => {
-            console.log(response.headers, "response headers");
+            // console.log(response.headers, "response headers");
             expect(response.status).toBe(200);
+            expect(response.type).toMatch(/json/i);
           });
       });
   });
